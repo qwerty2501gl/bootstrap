a7053dfbe473311a699b67de29748227bda698ea
qwerty2501gl <939468+qwerty2501gl@users.noreply.github.com>
2026-05-02 19:20:04 +0900

    change github

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,6 +3,6 @@
 sudo pacman -Syu --noconfirm
 sudo pacman -S --noconfirm git chezmoi
 
-read -p "create ssh key and set to gitlab."
+read -p "Please create ssh key and set to gitlab."
 
-chezmoi init --apply git@gitlab.com:qwerty2501gl/dotfiles
+chezmoi init --apply qwerty2501gl
